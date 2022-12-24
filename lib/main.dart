3ef24059da9c0878b72b31c720ff1c7517d4b181
e1230 <embeltrans@correo.udistrital.edu.co>
2022-12-24 00:27:29 -0500

    homePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:band_names/pages/home.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -9,14 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      initialRoute: 'home',
+      routes: {'home': (_) => HomePage()},
     );
   }
 }
